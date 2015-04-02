e7e8741fab56ed1029619adf602d974d1268b3ac
Sorunome <mail@sorunome.de>
2015-04-02 02:24:24 +0200

    untested clear eeprom changes

--- libraries/Gamebuino/examples/4.Utilities/Loader/saveeeprom.ino
+++ libraries/Gamebuino/examples/4.Utilities/Loader/saveeeprom.ino
@@ -11,38 +11,41 @@ void saveeeprom(){
     gb.display.println(F("Nothing to be saved!"));
     return;
   }
+  strcpy(completeName, prevGameName);
+  strcat(completeName, ".SAV");
+  
   gb.display.clear();
   gb.display.print(F("Saving EEPROM to\n"));
-  gb.display.print(prevGameName);
-  gb.display.println(F(".SAV\n"));
-  /*gb.display.println(F("\25:yes \26:no"));
+  gb.display.print(completeName);
+  
+  
+  gb.display.println(F("\n\25:yes \26:no"));
   gb.display.update();
   while(1){
     gb.buttons.update();
     if(gb.buttons.pressed(BTN_A)) break;
     if(gb.buttons.pressed(BTN_B)) return;
     delay(50);
-  }*/
+  }
 
 
 
-  strcpy(completeName, prevGameName);
-  strcat(completeName, ".SAV");
+  
   //to ask confirmation before overwriting existing saves
-  /*if(file.exists(completeName)){
+  if(file.exists(completeName)){
     gb.display.println(F("Overwrite existing?"));
     gb.display.println(F("\25:yes \26:no"));
     gb.display.update();
     while(1){
       gb.buttons.update();
       if(gb.buttons.pressed(BTN_A)){
-        file.delFile(completeName);
+        //file.delFile(completeName);
         break;
       }
       if(gb.buttons.pressed(BTN_B)) return;
       delay(50);
     }
-  }*/
+  }
   if(file.exists(completeName)){
     file.delFile(completeName);
   }
@@ -53,8 +56,11 @@ void saveeeprom(){
     for(byte i=0; i< 1024/BUFFER_SIZE; i++){
       buffer[BUFFER_SIZE+1] = '\0';
       for(byte j = 0; j<BUFFER_SIZE; j+=2){
-        buffer[j] = 0x0F | EEPROM.read((i*BUFFER_SIZE+j)/2);
-        buffer[j+1] = 0xF0 |  EEPROM.read((i*BUFFER_SIZE+j)/2);
+        byte b = EEPROM.read((i*BUFFER_SIZE+j)/2);
+        buffer[j] = 0x0F | b;
+        buffer[j+1] = 0xF0 | b;
+        //buffer[j] = 0xFF;
+        //buffer[j+1] = 0xFF;
       }
       file.writeLn(buffer);
     }
@@ -66,6 +72,15 @@ void saveeeprom(){
   else{
     gb.display.println(F("Error"));
     gb.display.update();
+  }
+  gb.display.println(F("\25:continue"));
+  gb.display.update();
+  while(1){
+    gb.buttons.update();
+    if(gb.buttons.pressed(BTN_A)){
+      break;
+    }
+    delay(50);
   }
 }
 
