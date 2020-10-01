cf44607846237b987de27f007408a033e40c8d13
SamVishwakarma <72178452+SamVishwakarma@users.noreply.github.com>
2020-10-01 08:30:12 +0530

    Update a_Hello.ino

--- examples/1.Basics/a_Hello/a_Hello.ino
+++ examples/1.Basics/a_Hello/a_Hello.ino
@@ -1,31 +1,31 @@
-//imports the SPI library (needed to communicate with Gamebuino's screen)
+//Imports the SPI library (needed to communicate with Gamebuino's screen)
 #include <SPI.h>
-//imports the Gamebuino library
+//Imports the Gamebuino library
 #include <Gamebuino.h>
-//creates a Gamebuino object named gb
+//Creates a Gamebuino object named gb
 Gamebuino gb;
 
-// the setup routine runs once when Gamebuino starts up
+// The setup routine runs once when Gamebuino starts up
 void setup(){
-  // initialize the Gamebuino object
+  // Initialize the Gamebuino object
   gb.begin();
-  //display the main menu:
+  //Display the main menu:
   gb.titleScreen(F("My first game"));
   gb.popup(F("Let's go!"), 100);
 }
 
-// the loop routine runs over and over again forever
+// The loop routine runs over and over again forever
 void loop(){
-  //updates the gamebuino (the display, the sound, the auto backlight... everything)
-  //returns true when it's time to render a new frame (20 times/second)
+  //Updates the gamebuino (the display, the sound, the auto backlight... everything)
+  //Returns true when it's time to render a new frame (20 times/second)
   if(gb.update()){
-    //prints Hello World! on the screen
+    //Prints Hello World! on the screen
     gb.display.println(F("Hello World!"));
-    //declare a variable named count of type integer :
+    //Declare a variable named count of type integer :
     int count;
-    //get the number of frames rendered and assign it to the "count" variable
+    //Get the number of frames rendered and assign it to the "count" variable
     count = gb.frameCount;
-    //prints the variable "count"
+    //Prints the variable "count"
     gb.display.println(count);
   }
 }
